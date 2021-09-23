ae112e1da0c018d139022f333a5f8ae02ac44a91
Joao Vitor Alves Lima <joao-mex@hotmail.com>
2021-09-22 23:26:18 -0300

    Service layer, component registration

--- src/main/java/com/joaoval/course/repositories/UserRepository.java
+++ src/main/java/com/joaoval/course/repositories/UserRepository.java
@@ -3,6 +3,7 @@ package com.joaoval.course.repositories;
 import com.joaoval.course.entities.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+
 public interface UserRepository extends JpaRepository<User, Long> {
 
 }
